deca3af76c2bc0856fe112ffe98ccaaa801eeda4
ydede404 <dakitse@gmail.com>
2017-09-12 18:47:31 +0200

    static pages générés

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/concept'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/cgv'
+
+  get 'static_pages/blog'
+
+  get 'static_pages/faq'
+
   devise_for :users
   devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
